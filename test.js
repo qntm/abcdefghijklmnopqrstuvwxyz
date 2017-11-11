f8d483ee408a64840a1acff47ab983e6673e72b7
qntm <qntm@users.noreply.github.com>
2017-11-11 01:15:13 +0000

    Tests!
    * Start using JavaScript Standard Style.
    * Start using Jasmine and add one unit test.
    * Start using `.editorconfig`.
    * Rename `index.js` to `abcdefghijklmnopqrstuvwxyz.js` for clarity.
    * abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz
    abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz
    abcdefghijklmnopqrstuvwxyz

---
+++ test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/* eslint-env jasmine */
+
+const abcdefghijklmnopqrstuvwxyz = require('./abcdefghijklmnopqrstuvwxyz.js')
+
+describe('`abcdefghijklmnopqrstuvwxyz`', () => {
+  it('is identical to \'abcdefghijklmnopqrstuvwxyz\'', () => {
+    expect(abcdefghijklmnopqrstuvwxyz).toBe('abcdefghijklmnopqrstuvwxyz')
+  })
+})
