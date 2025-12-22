85a7effe756be923667cebee562a3e620875b049
qntm <qntm@users.noreply.github.com>
2025-12-22 00:17:33 +0000

    Update abcdefghijklmnopqrstuvwxyz.test.js (#50)

--- test/abcdefghijklmnopqrstuvwxyz.test.js
+++ test/abcdefghijklmnopqrstuvwxyz.test.js
@@ -1,13 +1,13 @@
-import assert from 'node:assert'
+import assert from 'node:assert/strict'
 import { describe, it } from 'node:test'
 
 import { abcdefghijklmnopqrstuvwxyz } from '../src/abcdefghijklmnopqrstuvwxyz.js'
 
 describe('`abcdefghijklmnopqrstuvwxyz`', () => {
   it('is \'abcdefghijklmnopqrstuvwxyz\'', () => {
-    assert.deepStrictEqual(abcdefghijklmnopqrstuvwxyz, 'abcdefghijklmnopqrstuvwxyz')
+    assert.deepEqual(abcdefghijklmnopqrstuvwxyz, 'abcdefghijklmnopqrstuvwxyz')
   })
   it('has a length of 26 characters', () => {
-    assert.deepStrictEqual(abcdefghijklmnopqrstuvwxyz.length, 26)
+    assert.deepEqual(abcdefghijklmnopqrstuvwxyz.length, 26)
   })
 })
